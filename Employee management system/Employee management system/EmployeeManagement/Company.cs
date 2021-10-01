791cf99b838cd8e739aba4642447f8c19828888a
Muchere Brian <61405008+Brian-2000@users.noreply.github.com>
2021-10-01 23:55:50 +0300

    Add files via upload

---
+++ Employee management system/Employee management system/EmployeeManagement/Company.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_management_system.EmployeeManagement
+{
+    class Company
+    {
+        static void Main()
+
+        {
+            Employee brian = new Employee("Brian Muchere", 8);
+            Employee joan = new Employee("Joan Nanzala", 6);
+            Employee daniel = new Employee("Daniel Kiptoo", 7);
+            Employee bill = new Employee("Bill Andani", 4);
+            Employee lydia = new Employee("Lydia Kerubo", 7);
+            Employee susan = new Employee("Susan Wanjiru", 6);
+
+            department sales = new department("Company Sales");
+
+            sales.AddEmployee(bill);
+            sales.AddEmployee(joan);
+            sales.AddEmployee(daniel);
+            sales.describe();
+
+            department IT = new department("Company IT");
+            IT.AddEmployee(brian);
+            IT.AddEmployee(lydia);
+            IT.AddEmployee(susan);
+            IT.describe();
+
+        }
+    }
+}
---
+++ Employee management system/Employee management system/EmployeeManagement/department.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_management_system.EmployeeManagement
+{
+    class department
+    {
+        private string deptName;
+        private double deptBudget;
+
+        //int[] x = new int[6]
+        int counter;
+        Employee[] emps = new Employee[6];
+
+        public void AddEmployee(Employee obj)
+        {
+            emps[counter] = obj;
+            counter++;
+
+            if(obj.empGrade > 5)
+            {
+                this.deptBudget += 150000;
+             
+            }
+            else
+            {
+                this.deptBudget += 100000;
+            }
+
+        }
+        public void describe()
+        {
+            string temp = "Department : " + this.deptName + 
+                "\nTotal budget is :" + this.deptBudget + 
+                "\nEmployee :";
+            
+            foreach (Employee t in emps)
+            {
+                if (t != null)
+                {
+                    temp += t.ReturnNameGrade() + "";
+                }
+            }
+            Console.WriteLine(temp);
+        }
+
+        public department(string depName)
+        {
+            this.deptName = deptName;
+            this.deptBudget = 50000;
+        }
+
+    }
+}
